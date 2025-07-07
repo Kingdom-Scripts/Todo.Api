87cdda99978335f1c84aed595230ea81b1f85a54
Goodnews Azonubi <azonubigoodnews@gmail.com>
2025-07-07 20:18:28 +0100

    Update: Database connection successfull and endpoints working for get-all-todos & create-todo

--- Todo.Api/Program.cs
+++ Todo.Api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<TodoDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("TodoDbConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<TodoService>();
